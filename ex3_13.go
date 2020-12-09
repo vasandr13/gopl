ade3a7050e43dc96938c3a40f9477045de004b5e
Andrey Vasilev <an_vasilev@inbox.ru>
2020-12-09 17:00:43 +0300

    Update ex3_13.go

--- ex3_13.go
+++ ex3_13.go
@@ -1,15 +1,19 @@
+
 // took it from here:
 // https://golang.org/doc/effective_go.html#constants
+// took explanation of bitwise operator from here:
+// https://stackoverflow.com/questions/5801008/go-and-operators
+
 
 package main
 
 import "fmt"
 
 const (
-	_  = iota             // 0
-	KB = 1 << (10 * iota) // 1 << (10 * 1) = 1 times 2 (ten times) = 1024
-	MB                    // 1 << (10 * 2) = 1 times 2 (twenty times) = 1048576
-	GB
+	_  = iota             	// 0
+	KB = 1 << (10 * iota) 	// 1 << (10 * 1) = (1 times 2) ten times = 1024
+	MB                    	// 1 << (10 * 2) = (1 times 2) twenty times = 1048576
+	GB			// 1 << (10 * 3) = (1 times 2) thirty times = 1073741824
 	TB
 	PB
   	EB
