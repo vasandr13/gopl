a889bad12aec73f194c7b18a3520c625f0eb6ff5
Andrey Vasilev <an_vasilev@inbox.ru>
2020-12-09 16:55:05 +0300

    Create ex3_13.go

---
+++ ex3_13.go
@@ -0,0 +1,24 @@
+// took it from here:
+// https://golang.org/doc/effective_go.html#constants
+
+package main
+
+import "fmt"
+
+const (
+	_  = iota             // 0
+	KB = 1 << (10 * iota) // 1 << (10 * 1) = 1 times 2 (ten times) = 1024
+	MB                    // 1 << (10 * 2) = 1 times 2 (twenty times)
+	GB
+	TB
+	PB
+  EB
+)
+
+func main() {
+  fmt.Println(KB, MB, GB, TB, PB, EB)
+}
+
+
+// go run ex3_13.go 
+// 1024 1048576 1073741824 1099511627776 1125899906842624 1152921504606846976
